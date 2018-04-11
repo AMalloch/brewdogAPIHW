d6ebcf4ac8cbcd544e87749a687dcd8f38f618ab
Angus Malloch <angusbass@hotmail.com>
2018-04-11 17:47:09 +0100

    Added a callback function called requestComplete that takes in the responce text and parses it.

--- public/index.js
+++ public/index.js
@@ -8,6 +8,12 @@ const makeRequest = function(url, callback){
   request.send();
 };
 
+const requestComplete = function(){
+  if(this.status !== 200) return;
+  const jsonString = this.responseText;
+  const beers = JSON.parse(jsonString);
+}
+
 var app = function(){
 
   const url = "https://s3-eu-west-1.amazonaws.com/brewdogapi/beers.json";
