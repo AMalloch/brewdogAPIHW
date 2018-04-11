e0d1fada31629545e3a00dfc399a4524650e12a7
Angus Malloch <angusbass@hotmail.com>
2018-04-11 17:43:29 +0100

    Added a makeRequest function and a url in app  calling the function. However no call back function atm.

--- public/index.js
+++ public/index.js
@@ -1,6 +1,17 @@
+const makeRequest = function(url, callback){
+  const request = new XMLHttpRequest();
 
+  request.open("GET", url);
+
+  request.addEventListener("load", callback);
+
+  request.send();
+};
 
 var app = function(){
+
+  const url = "https://s3-eu-west-1.amazonaws.com/brewdogapi/beers.json";
+  makeRequest(url, requestComplete);
 
 }
 
